e0de0924fe9ce90b433b989236c949a48fd67e12
jung <doizz@naver.com>
2022-05-11 22:02:44 +0900

    Initial commit

---
+++ test/addNumberTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class addNumberTest {
+
+    @Test
+    public void 덧셈(){
+
+    }
+
+    @Test
+    public void 뺄셈(){
+
+    }
+
+}
