968cabd76e022046d7618897a1ed071b5ab038f0
doizz <doizz@naver.com>
2022-06-02 22:12:53 +0900

    테스트코드 작업중

--- test/addNumberTest.java
+++ test/addNumberTest.java
@@ -1,17 +1,51 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class addNumberTest {
 
-    @Test
-    public void 덧셈(){
+    @ParameterizedTest
+    @DisplayName("공백 문자열이 있으면 split 으로 문자를 나누는 테스트")
+    @ValueSource(strings = {"1 3 4 5"})
+    public void 문자열나누기(String input){
+        String[] array = input.split(" ");
+        System.out.println(input);
 
     }
 
     @Test
-    public void 뺄셈(){
+    @DisplayName("null 이거나 빈 문자열일 경우 예외처리 테스트")
+    public void 문자열예외처리(){
 
     }
+
+    @Test
+    @DisplayName("숫자만 있는 경우 예외처리 테스트")
+    public void 숫자만들어올경우(){
+
+    }
+    @Test
+    @DisplayName("연산자만 있는 경우 예외처리 테스트")
+    public void 연산자만들어올경우(){
+
+    }
+    @Test
+    @DisplayName("계산 가능한 식인지 테스트(1. 홀수자리는 피연산자, 짝수자리는 연산자)")
+    public void 계산신연산처리(){
+
+    }
+    @Test
+    @DisplayName("계산 가능한 식인지 테스트(2. 피연산자와 연산자의 갯수의 합은 무조건 홀수다)")
+    public void 계산식판별(){
+
+    }
+    @Test
+    @DisplayName("덧셈, 뺄셈, 곱셈, 나눗셈 테스트")
+    public void 산술테스트(){
+
+    }
+
 
 }
